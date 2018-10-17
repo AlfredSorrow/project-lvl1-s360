bdb7fc9cf498d94afb26335be34fba7f2d4aaecc
Alfred Sorrow <kurtsorrow@gmail.com>
2018-10-17 20:06:44 +0300

    Lint fixes

--- src/Cli.php
+++ src/Cli.php
@@ -3,8 +3,9 @@ namespace BrainGames\Cli;
 
 use function \cli\line;
 use function \cli\prompt;
+
 function run($gameDescripton = '')
-{   
+{
     line('Welcome to the Brain Games!');
     $gameDescripton === ''? line('') : line($gameDescripton . "\n");
     $name = \cli\prompt('May I have your name?');
